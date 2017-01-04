1737af4515b4ba911d960858367c736ca44b2d2a
Alexander Kovalkov <akovalkov@yandex.ru>
2017-01-04 05:05:23 -0500

    Add Block class

--- m4_common.py
+++ m4_common.py
@@ -44,4 +44,58 @@ class Token:
             return "%s (%s)" % (types[self.type - self.TOKEN_EOF], self.data)
 
 
+class Block:
+    INPUT_STRING = 0    # String resulting from macro expansion.
+    INPUT_FILE = 1      # File from command line or include.
+    INPUT_MACRO = 2     # Builtin resulting from defn.
+
+    CHAR_EOF = "-1"   # character return on EOF 
+    CHAR_MACRO = "-2" # character return for MACRO token 
+
+    def __init__(self, type, arg1, arg2 = None):
+        self.type = type
+        self.line = 1
+        self.offset = 0
+        self.start_of_input_line = False
+        if type == self.INPUT_FILE:
+            self.name = arg1
+            self.content = self.read_file(arg2)
+        elif type == self.INPUT_STRING:
+            self.name = None
+            self.content = arg1
+        elif type == self.INPUT_MACRO:
+            self.name = None
+            self.content = arg1
+        else:
+            raise Exception("Unknown input block type %d" % type)
+
+    def read_file(self, filepath):
+        return open(filepath).read()
+
+    def next_symbol(self):
+        # check new line start
+        if self.type == self.INPUT_FILE and self.start_of_input_line:
+            self.start_of_input_line = False
+            self.line += 1 # track line number only for file
+            #print("line: %d" % self.line)
+        # check end of content
+        if self.offset >= len(self.content):
+            return self.CHAR_EOF
+        symbol = self.content[self.offset]
+        if self.type == self.INPUT_FILE and symbol == '\n': # next symbol start a new line
+            self.start_of_input_line = True
+        self.offset += 1
+        return symbol   
+
+    def peek_symbol(self, shift = 0):
+        if self.offset + shift >= len(self.content):
+            return self.CHAR_EOF
+        return self.content[self.offset + shift]
+
+    def __str__(self):
+        types = ['INPUT_STRING','INPUT_FILE','INPUT_MACRO']
+        if self.name:
+            return "%s name %s line %d" % (types[self.type - self.INPUT_STRING], self.name, self.line)
+        else:
+            return "%s line %d" % (types[self.type - self.INPUT_STRING], self.line)
 
